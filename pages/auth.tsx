f40cdcfbbe26fe3771c591d0071c49cba6e90980
Jean Yves Ehouman <yvesehouman09@gmail.com>
2024-04-06 03:20:02 +0900

    Login and register functions with mongodb

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,5 +1,9 @@
 import Input from "@/components/Input";
 import { useCallback, useState } from "react";
+import axios from 'axios';
+import { signIn } from "next-auth/react";
+
+
 
 const Auth = () => {
 
@@ -12,6 +16,44 @@ const Auth = () => {
     const toggleVariant = useCallback( () => {
         setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login')
     }, []);
+
+    const register = useCallback(async () => {
+        try {
+
+            await axios.post('api/register', {
+                email,
+                name,
+                password
+            });
+            
+        } catch (error) {
+
+            console.log(error);
+            
+            
+        }
+    }, [email, name, password]);
+
+
+    const login = useCallback(async () => {
+        try {
+
+            
+            await signIn('credentials', {
+                email,
+                password,
+                redirect: false,
+                callbackUrl: '/'
+            });
+            
+        } catch (error) {
+
+            console.log(error);
+            
+            
+        }
+    }, [email, password]);
+
 
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -49,8 +91,8 @@ const Auth = () => {
                                 value={ password }
                             />
                         </div>
-                        <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
-                            
+                        <button onClick={  variant === 'login' ? login :   register } className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                        
                             { variant === 'login' ? 'Login' : 'Sign up'}
                         </button>
                         <div className="text-neutral-500 mt-12">
