cff1e070a5a657215623173a46dda1bb09688968
Jean Yves Ehouman <yvesehouman09@gmail.com>
2024-04-07 02:47:11 +0900

    Github and google auth

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -2,10 +2,15 @@ import Input from "@/components/Input";
 import { useCallback, useState } from "react";
 import axios from 'axios';
 import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
 
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
 
 const Auth = () => {
+
+    const router = useRouter();
 
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
@@ -17,24 +22,6 @@ const Auth = () => {
         setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login')
     }, []);
 
-    const register = useCallback(async () => {
-        try {
-
-            await axios.post('api/register', {
-                email,
-                name,
-                password
-            });
-            
-        } catch (error) {
-
-            console.log(error);
-            
-            
-        }
-    }, [email, name, password]);
-
-
     const login = useCallback(async () => {
         try {
 
@@ -45,6 +32,8 @@ const Auth = () => {
                 redirect: false,
                 callbackUrl: '/'
             });
+
+            router.push('/');
             
         } catch (error) {
 
@@ -52,7 +41,30 @@ const Auth = () => {
             
             
         }
-    }, [email, password]);
+    }, [email, password, router]);
+
+
+    const register = useCallback(async () => {
+        try {
+
+            await axios.post('api/register', {
+                email,
+                name,
+                password
+            });
+
+            login();
+            
+        } catch (error) {
+
+            console.log(error);
+            
+            
+        }
+    }, [email, name, password, login]);
+
+
+    
 
 
     return (
@@ -95,6 +107,47 @@ const Auth = () => {
                         
                             { variant === 'login' ? 'Login' : 'Sign up'}
                         </button>
+
+                        <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+                            <div 
+                            className="
+                            w-10
+                            h-10
+                            bg-white
+                            rounded-full
+                            flex
+                            items-center 
+                            justify-center
+                            cursor-pointer
+                            hover:opacity-80
+                            transition
+                            "
+                            >
+                            <FcGoogle size={30}/>
+
+                            </div>
+
+                            <div 
+                            className="
+                            w-10
+                            h-10
+                            bg-white
+                            rounded-full
+                            flex
+                            items-center 
+                            justify-center
+                            cursor-pointer
+                            hover:opacity-80
+                            transition
+                            "
+                            >
+                            <FaGithub size={30}/>
+
+                            </div>
+
+                        </div>
+
+
                         <div className="text-neutral-500 mt-12">
 
                         { variant === 'login' ? 'First time using Netflix ?' : 'Already have an account ?'}
