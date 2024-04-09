0310881d9ba326dce98c0beec3b39659e50dd3dc
Jean Yves Ehouman <yvesehouman09@gmail.com>
2024-04-09 13:24:34 +0900

    Menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import NavbarItem from './NavbarItem';
+
+import { BsChevronDown , BsSearch, BsBell} from "react-icons/bs";
+import MobileMenu from './MobileMenu';
+import AccountMenu from './AccountMenu';
+
 
 export default function Navbar() {
 
+const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+const toggleMobileMenu = useCallback(() => {
+    setShowMobileMenu((currentValue) => !currentValue);
+}, [])
 
   return (
     <nav className='w-full fixed z-40'>
@@ -28,7 +38,7 @@ export default function Navbar() {
                 lg:flex
             "
             >
-                
+
                 <NavbarItem label='Home' />
                 <NavbarItem label='Series' />
                 <NavbarItem label='Films' />
@@ -37,6 +47,29 @@ export default function Navbar() {
                 <NavbarItem label='Browse by languages' />
 
             </div>
+            
+            <div onClick={ toggleMobileMenu } className='lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative'>
+                <p className='text-white text-sm'>Browse</p>
+                <BsChevronDown  className='text-white transition'/>
+                <MobileMenu  visible={ showMobileMenu} />
+            </div>
+
+            <div className='flex flex-row ml-auto gap-7 items-center'>
+                <div className='text-gray-200 hover:text-gray-300 cursor-pointer transition'>
+                    <BsSearch/>
+                </div>
+                <div className='text-gray-200 hover:text-gray-300 cursor-pointer transition'>
+                    <BsBell/>
+                </div>
+                <div className='flex flex-row items-center gap-2 cursor-pointer relative'>
+                    <div className='w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden'>
+                        <img src="/images/default-blue.png" alt="" />
+                    </div>
+                    <BsChevronDown  className='text-white transition'/>
+                    <AccountMenu/>
+                </div>
+            </div>
+            
 
         </div>
     </nav>
